7a66fef450b7f3a15ce36e70da9134d8a82c894a
Inan Evin <inanevin@gmail.com>
2025-01-30 17:53:55 +0200

    project: remove phmap from deps

--- StakeforgeSDK/Dependencies/phmap/link.cmake
+++
@@ -1,8 +0,0 @@
-macro(link_phmap)
-
-   set(DIRNAME "phmap")
-
-   target_include_directories(${PROJECT_NAME} SYSTEM PUBLIC ${CMAKE_SOURCE_DIR}/Dependencies/${DIRNAME}/include)
-
-   message("${PROJECT_NAME} -> ${DIRNAME} has been linked.")
-endmacro()
